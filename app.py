d4465adbd3cc4dc3d672e867e5367baa243fdda1
y_r <yanis.remmache@gmail.com>
2024-12-23 15:05:18 +0100

    add requirements

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ from concurrent.futures import ThreadPoolExecutor
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
-# Initialisation de l'application Dash
+# Init Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 app.title = "Eventbrite App"
 server = app.server
 
-# Mise en place des layouts pour les pages
+# Layouts
 app.layout = dbc.Container([
     dcc.Location(id='url', refresh=False),
     dcc.Store(id='token-store'),
@@ -25,7 +25,7 @@ app.layout = dbc.Container([
     html.Div(id='error-message', style={'color': 'red'})  # For displaying error messages
 ], fluid=True)
 
-# Callback pour gérer la navigation entre les pages
+# Callback for pages navigation
 @app.callback(
     Output('page-content', 'children'),
     Input('url', 'pathname')
@@ -35,11 +35,11 @@ def display_page(pathname):
         return main_page()
     return auth_page()
 
-# Callback pour vérifier le token et rediriger l'utilisateur
+# Callback to check token
 @app.callback(
     [Output('url', 'pathname'),
      Output('error-message', 'children'),
-     Output('token-store', 'data')],  # Stocker le token
+     Output('token-store', 'data')],  # Store token
     Input('submit-button', 'n_clicks'),
     State('token-input', 'value')
 )
@@ -56,26 +56,25 @@ def authenticate(n_clicks, token):
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            # Token valide
-            return '/main', '', token  # Retourne le token
+            # Valid token
+            return '/main', '', token
         elif response.status_code == 429:
             # Rate Limit
             return dash.no_update, "Hourly rate limit has been reached for this token", None
         else:
-            # Token invalide
+            # Invalid token
             return dash.no_update, "Invalid token", None
     except Exception as e:
-        # Erreur dans la requête
         return dash.no_update, "An error occurred. Please try again.", None
 
 
-# Callback pour filtrer les événements
+# Callback to filter events
 @app.callback(
     Output('event-list', 'children'),
     Input('filter-button', 'n_clicks'),
     State('start-date-picker', 'date'),
     State('end-date-picker', 'date'),
-    State('token-store', 'data')  # Récupérer le token stocké
+    State('token-store', 'data')  # Get stored token
 )
 def filter_events(n_clicks, start_date, end_date, token):
     if n_clicks == 0:
@@ -90,10 +89,10 @@ def filter_events(n_clicks, start_date, end_date, token):
     try:
         events = get_filter_events_organization(token=token,
                                                 start_date=start_date,
-                                                end_date=end_date)  # Passer le token
+                                                end_date=end_date)
         event_names = extract_list_name_events(events)
         options = [{'label': name, 'value': name} for name in event_names]
-        options.insert(0, {'label': 'All', 'value': 'All'})  # Ajouter l'option "All"
+        options.insert(0, {'label': 'All', 'value': 'All'})  # "All" option
         return dbc.Card([
             dbc.CardBody([
                 dcc.Checklist(
@@ -111,7 +110,7 @@ def filter_events(n_clicks, start_date, end_date, token):
         return html.Div(f"An error occurred: {str(e)}", style={'color': 'red'})
 
 
-# Callback pour gérer la sélection de "All"
+# Callback to manage "All" selection
 @app.callback(
     Output('event-selector', 'value'),
     Input('event-selector', 'value'),
@@ -120,15 +119,14 @@ def filter_events(n_clicks, start_date, end_date, token):
 def select_all_events(selected_values, options):
     all_values = [option['value'] for option in options if option['value'] != 'All']
     if 'All' in selected_values:
-        # Si "All" est sélectionné, soit tout sélectionner soit tout désélectionner
         if set(all_values).issubset(selected_values):
-            return []  # Tout désélectionner
+            return []
         else:
-            return all_values  # Tout sélectionner
+            return all_values
     return selected_values
 
 
-# Fonction pour afficher les événements sélectionnés et les participants
+# Display selected events and attendees
 def display_selected_events(selected_event_names, token, start_date, end_date):
     if not selected_event_names or not token:
         return html.Div("No events selected or token missing.", style={'color': 'red'})
@@ -136,33 +134,30 @@ def display_selected_events(selected_event_names, token, start_date, end_date):
     try:
         response = get_filter_events_organization(token=token,
                                                   start_date=start_date,
-                                                  end_date=end_date)  # Récupérer les événements
+                                                  end_date=end_date)
         selected_events = [
             event for event in response
             if event["name"]["text"] in selected_event_names
         ]
 
-        # Étape 2 : Extraction des informations des événements
+        # Extract events' informations
         list_event_infos = [extract_event_informations(event) for event in selected_events]
         df_event_infos = pd.DataFrame(list_event_infos)
 
-        # Étape 3 : Extraction des informations des participants en parallèle
+        # Extract attendees' informations
         def fetch_attendee_data(event_id):
             """Fetch and process attendee data for a single event."""
             attendees_data = get_event_attendees(token, event_id)
             return extract_attendee_informations(attendees_data)
 
-        # Utilisation de ThreadPoolExecutor pour paralléliser les appels API
         with ThreadPoolExecutor() as executor:
             list_df_infos_attendees_per_event = list(
                 executor.map(fetch_attendee_data, df_event_infos["Event ID"])
             )
 
-        # Concaténation des DataFrames des participants
         filtered_list_df_infos_attendees_per_event = [item for item in list_df_infos_attendees_per_event if not isinstance(item, list)]
         df_infos_attendees_per_event = pd.concat(filtered_list_df_infos_attendees_per_event, axis=0, ignore_index=True)
 
-        # Étape 4 : Jointure gauche entre les informations des événements et des participants
         df_infos_events_and_attendees = pd.merge(
             df_event_infos, 
             df_infos_attendees_per_event, 
@@ -172,7 +167,7 @@ def display_selected_events(selected_event_names, token, start_date, end_date):
 
         columns = df_infos_events_and_attendees.columns.tolist()
 
-        # Création de du barplot de l'âge
+        # Age barplot
         age_category_counts = df_infos_events_and_attendees['Age'].value_counts(normalize=True).reset_index()
         age_category_counts.columns = ['Age', 'Frequency']
         age_category_counts['Frequency'] = age_category_counts['Frequency'] * 100
@@ -187,7 +182,8 @@ def display_selected_events(selected_event_names, token, start_date, end_date):
             color_continuous_scale='Viridis'
         )
         age_barplot.update_layout(showlegend=False)
-        # Création du barplot pour le genre
+
+        # Gender barplot
         gender_category_counts = df_infos_events_and_attendees['Gender'].value_counts(normalize=True).reset_index()
         gender_category_counts.columns = ["Gender", "Frequency"]
         gender_category_counts['Frequency'] = gender_category_counts['Frequency'] * 100
@@ -200,7 +196,7 @@ def display_selected_events(selected_event_names, token, start_date, end_date):
             color_discrete_map={'male': 'blue', 'female': 'pink'}
         )
 
-        # Retourner le tableau et les graphiques
+        # Display table and graphes
         return html.Div([
             dbc.Row([
                 dbc.Col(dcc.Dropdown(
@@ -238,7 +234,6 @@ def display_selected_events(selected_event_names, token, start_date, end_date):
             ]),
             dcc.Download(id="download-dataframe"),
 
-            # Ajouter l'histogramme de l'âge et le barplot du genre
             dbc.Row([
                 dbc.Col(dcc.Graph(figure=age_barplot), width=6),
                 dbc.Col(dcc.Graph(figure=gender_barplot), width=6)
@@ -251,7 +246,7 @@ def display_selected_events(selected_event_names, token, start_date, end_date):
 
 
 
-# Callback pour mettre à jour le tableau en fonction des colonnes sélectionnées
+# Callback to update the table
 @app.callback(
     Output('attendees-table', 'columns'),
     Input('column-selector', 'value'),
@@ -263,13 +258,13 @@ def update_table_columns(selected_columns, data):
     return [{"name": col, "id": col} for col in selected_columns]
 
 
-# Callback unique pour exporter le tableau en CSV ou XLSX
+# Callback to export table as csv or xlsx
 @app.callback(
     Output("download-dataframe", "data"),
     [Input("export-csv-button", "n_clicks"),
      Input("export-xlsx-button", "n_clicks")],
-    State("attendees-table", "data"),  # Récupérer les données actuelles du tableau
-    State("column-selector", "value"),  # Colonnes visibles sélectionnées par l'utilisateur
+    State("attendees-table", "data"),
+    State("column-selector", "value"),
     State("start-date-picker", "date"),
     State("end-date-picker", "date"),
     prevent_initial_call=True
@@ -278,7 +273,6 @@ def export_table(n_clicks_csv, n_clicks_xlsx, data, selected_columns, start_date
     if not data or not selected_columns:
         raise PreventUpdate
 
-    # Détection du bouton cliqué
     ctx = dash.callback_context
     if not ctx.triggered:
         raise PreventUpdate
@@ -286,10 +280,8 @@ def export_table(n_clicks_csv, n_clicks_xlsx, data, selected_columns, start_date
     button_id = ctx.triggered[0]['prop_id'].split('.')[0]
     df = pd.DataFrame(data)
 
-    # Filtrer les données pour ne garder que les colonnes sélectionnées
     df_filtered = df[selected_columns]
 
-    # Exportation
     if button_id == "export-csv-button":
         filename = f"custom_report_{start_date}_{end_date}.csv"
         return dcc.send_data_frame(df_filtered.to_csv, filename, index=False, encoding='utf-8-sig')
@@ -300,18 +292,18 @@ def export_table(n_clicks_csv, n_clicks_xlsx, data, selected_columns, start_date
         return html.Div("Error during export. Please try again.", style={'color': 'red'})
 
 
-# Callback pour afficher les événements sélectionnés et les participants
+# Callback to display selected events and attendees
 @app.callback(
     Output('event-details', 'children'),
-    Input('validate-button', 'n_clicks'),  # Déclenchement par le bouton "Validate"
-    State('event-selector', 'value'),      # Événements sélectionnés
+    Input('validate-button', 'n_clicks'),
+    State('event-selector', 'value'),
     State('token-store', 'data'),
     State('start-date-picker', 'date'),
     State('end-date-picker', 'date')
 )
 def display_event_details(n_clicks, selected_event_names, token, start_date, end_date):
     if n_clicks == 0:
-        raise PreventUpdate  # Ne rien faire tant que le bouton n'est pas cliqué
+        raise PreventUpdate
 
     if selected_event_names:
         return display_selected_events(selected_event_names, token, start_date, end_date)
